08bae2dda476e67a208d8de3b76066f80c2b9991
mbrsagor <mbrsagor@gmail.com>
2021-06-16 22:25:47 +0600

    Featues: API installation

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+
+#
+# @app.route('/')
+# def hello_world():
+#     return render_template('index.html')
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "World"}
 
+
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run()
