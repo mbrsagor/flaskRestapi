6fcb09642732518a5032a09f5996e98838b3c902
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2023-09-03 20:57:19 +0600

    New: add new book ORM

--- main.py
+++ main.py
@@ -23,6 +23,28 @@ class Product(db.Model):
     UniqueConstraint('discount_price', 'title', name='user_product_unique')
 
 
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Book(Base):
+   __tablename__ = 'books'
+   id = Column(Integer, primary_key=True)
+   title = Column(String)
+   author = Column(String)
+   engine = create_engine('sqlite:///books.db')
+   Session = sessionmaker(bind=engine)
+   session = Session()
+
+# Create a new book
+new_book = Book(title="Django Unleashed", author="Andrew Pinkham")
+session.add(new_book)
+session.commit()
+
+
+
 class Index(Resource):
     def get(self):
         data = [
